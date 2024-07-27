1cce930bc4eb52fd26c2d1f2de4461d7d7e13bb5
Agata Kamińska <irbesarta@gmail.com>
2024-07-27 12:31:47 +0200

    addingTextChangingButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("Kliknij przycisk, aby zmienić tekst");
+
+  const handleClick = () => {
+    setText("Tekst został zmieniony!");
+  };
 
   return (
-    <>
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,6 +26,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={handleClick}>{text}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,8 +34,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
